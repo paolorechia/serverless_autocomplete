3c8055af977bd85d455ae06bb16b41303591c3d6
Paolo Rechia <paolorechia@gmail.com>
2020-08-02 12:22:37 +0200

    Write more tests

--- lambda/levenshtein.py
+++ lambda/levenshtein.py
@@ -1,37 +1,62 @@
 import pytest
-from collections import namedtuple
 
 
 class DistanceMatrix:
     def __init__(self, a, b):
         self.a = a
         self.b = b
-        self.distance_matrix = self.create_distance_matrix(a, b)
+        self.distance_matrix = []
 
     def __str__(self):
+        if not self.distance_matrix:
+            return "a: {}\nb: {}\n [Empty Distance Matrix]".format(self.a, self.b)
+
+        assert isinstance(self.distance_matrix, list)
+
         header = "#"
         for c in self.a:
             header += " {}".format(c)
 
         i = 0
         result = header + "\n"
-        for row in self.distance_matrix:
+
+        # Build first row
+        result += " "
+        first_row = self.distance_matrix[0]
+        for n in first_row:
+            result += " {}".format(n)
+
+        result += "\n"
+
+        other_rows = self.distance_matrix[1:]
+        for row in other_rows:
             result += self.b[i]
             i += 1
             for n in row:
                 result += " {}".format(n)
             result += "\n"
-
         return result
 
-    def create_distance_matrix(self, a, b):
+    def create_distance_matrix(self):
         rows = []
         columns = []
-        for _ in range(len(a)):
+        for _ in range(len(self.a)):
             rows.append(0)
-        for _ in range(len(b)):
+        columns.append(rows[:])
+        for _ in range(len(self.b)):
             columns.append(rows[:])
-        return columns
+        self.distance_matrix = columns
+
+    def edit_distance(self):
+        # Check for empty strings
+        print("TODO")
+        if len(self.a) == 0:
+            return len(self.b)
+        if len(self.b) == 0:
+            return len(self.a)
+
+        # do more stuff here stuff
+        return 0
 
 
 @pytest.mark.parametrize(
@@ -60,14 +85,50 @@ def test_levenshtein(a, b, expected_distance):
     assert levenshtein(a, b) == expected_distance
 
 
+@pytest.mark.parametrize(
+    "a, b, expected_matrix",
+    [
+        (
+            "kitten",
+            "sitting",
+            [
+                [0, 1, 2, 3, 4, 5, 6],
+                [1, 1, 2, 3, 4, 5, 6],
+                [2, 2, 1, 2, 3, 4, 5],
+                [3, 3, 2, 1, 2, 3, 4],
+                [4, 4, 3, 2, 1, 2, 3],
+                [5, 5, 4, 3, 2, 2, 3],
+                [6, 6, 5, 4, 3, 3, 2],
+                [7, 7, 6, 5, 4, 4, 3],
+            ],
+        ),
+        (
+            "Saturday",
+            "Sunday",
+            [
+                [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                [1, 0, 1, 2, 3, 4, 5, 6, 7],
+                [2, 1, 1, 2, 2, 3, 4, 5, 6],
+                [3, 2, 2, 2, 3, 3, 4, 5, 6],
+                [4, 3, 3, 3, 3, 4, 3, 4, 5],
+                [5, 4, 3, 4, 4, 4, 4, 3, 4],
+                [6, 5, 4, 4, 5, 5, 5, 4, 3],
+            ],
+        ),
+    ],
+)
+def test_edit_distance(a, b, expected_matrix):
+    dm = DistanceMatrix(a, b)
+    dm.create_distance_matrix()
+    assert dm.distance_matrix == expected_matrix
+
+
 def levenshtein(a, b):
-    # Check for empty strings
-    if len(a) == 0:
-        return len(b)
-    if len(b) == 0:
-        return len(a)
-
-    return 0
+    dm = DistanceMatrix(a, b)
+    dm.create_distance_matrix()
+    return dm.edit_distance()
 
 
-print(DistanceMatrix("tree", "hop"))
+dm = DistanceMatrix("tree", "hop")
+dm.create_distance_matrix()
+print(dm)
